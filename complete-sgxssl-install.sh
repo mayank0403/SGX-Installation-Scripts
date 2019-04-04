77e491641a908cd2cc60081c537c48fed5c08960
Mayank <mayankrathee.japan@gmail.com>
2019-04-04 10:41:13 +0000

    Updated ssl script

--- complete-sgxssl-install.sh
+++ complete-sgxssl-install.sh
@@ -2,6 +2,8 @@
 
 echo "===>>>After copying the correct sgx ssl linux dir"
 
+sudo cp -r SGX-Installation-Scripts/sgx-ssl-linux-dir/* intel-sgx-ssl/Linux/.
+
 cd intel-sgx-ssl/Linux
 
 sudo make clean
